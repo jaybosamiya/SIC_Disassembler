7eda6b0e654351b0727031e7295c3883de9d4035
Jay Bosamiya <jaybosamiya@gmail.com>
2015-02-21 01:54:28 +0530

    Prevent code duplication

--- disassembler.cpp
+++ disassembler.cpp
@@ -29,6 +29,21 @@ bool disassemble(ifstream &ifile, ofstream &ofile) {
 	status("Done writing output file");
 }
 
+string read_hex_columns(ifstream &ifile, int col_begin, int col_end, char record_type = 'a') { // inclusive of both
+	string ret;
+	char t;
+	for ( int col = col_begin ; col <= col_end ; col++ ) {
+		t = ifile.get();
+		if ( t == EOF ) {
+			fatal("Unexpected end of " + record_type + string(" record"));
+		} else if ( !is_hex_digit(t) ) {
+			fatal("Unexpected character " + t + string(" in ") + record_type + string(" record"));
+		}
+		ret += t;
+	}
+	return ret;
+}
+
 bool read_record(ifstream &ifile, string &record) {
 	char t;
 	do {
@@ -41,25 +56,13 @@ bool read_record(ifstream &ifile, string &record) {
 	record += t;
 	switch (t) {
 		case 'H': // Header
-			for ( int col = 2 ; col <= 19 ; col++ ) {
-				t = ifile.get();
-				if ( t == EOF ) {
-					fatal("Unexpected end of Header Record");
-				}
-				record += t;
-			}
+			record += read_hex_columns(ifile,2,19,'H');
 			break;
 		case 'T': // Text
 			//TODO
 			break;
 		case 'E': // End
-			for ( int col = 2 ; col <= 7 ; col++ ) {
-				t = ifile.get();
-				if ( t == EOF ) {
-					fatal("Unexpected end of End Record");
-				}
-				record += t;
-			}
+			record += read_hex_columns(ifile,2,7,'E');
 			break;
 		default:
 			fatal("Unknown record type " + t);
