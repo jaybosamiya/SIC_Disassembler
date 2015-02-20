f231c3235cee49c1b01d2e98fd1038f5eb2f43c6
Jay Bosamiya <jaybosamiya@gmail.com>
2015-02-21 00:44:35 +0530

    Add reverse SYMTAB

--- symtab.cpp
+++ symtab.cpp
@@ -4,11 +4,13 @@
 using namespace std;
 
 map<string,string> symtab;
+map<string,string> revsymtab;
 
 bool add_to_symtab(std::string symbol, std::string value) {
 	if ( symtab.count(symbol) )
 		return false;
 	symtab[symbol] = value;
+	revsymtab[value] = symbol;
 	return true;
 }
 
@@ -19,30 +21,37 @@ bool read_from_symtab(std::string symbol, std::string &value) {
 	return true;
 }
 
+bool find_from_symtab(std::string &symbol, std::string value) {
+	if ( !revsymtab.count(value) )
+		return false;
+	symbol = revsymtab[value];
+	return true;
+}
+
 void initialize() {
-	symtab["ADD"]  = "18";
-	symtab["AND"]  = "40";
-	symtab["COMP"] = "28";
-	symtab["DIV"]  = "24";
-	symtab["J"]    = "3C";
-	symtab["JEQ"]  = "30";
-	symtab["JGT"]  = "34";
-	symtab["JLT"]  = "38";
-	symtab["JSUB"] = "48";
-	symtab["LDA"]  = "00";
-	symtab["LDCH"] = "50";
-	symtab["LDL"]  = "08";
-	symtab["LDX"]  = "04";
-	symtab["MUL"]  = "20";
-	symtab["OR"]   = "44";
-	symtab["RD"]   = "D8";
-	symtab["RSUB"] = "4C";
-	symtab["STA"]  = "0C";
-	symtab["STCH"] = "54";
-	symtab["STL"]  = "14";
-	symtab["STX"]  = "10";
-	symtab["SUB"]  = "1C";
-	symtab["TD"]   = "E0";
-	symtab["TIX"]  = "2C";
-	symtab["WD"]   = "DC";
+	add_to_symtab("ADD" ,"18");
+	add_to_symtab("AND" ,"40");
+	add_to_symtab("COMP","28");
+	add_to_symtab("DIV" ,"24");
+	add_to_symtab("J"   ,"3C");
+	add_to_symtab("JEQ" ,"30");
+	add_to_symtab("JGT" ,"34");
+	add_to_symtab("JLT" ,"38");
+	add_to_symtab("JSUB","48");
+	add_to_symtab("LDA" ,"00");
+	add_to_symtab("LDCH","50");
+	add_to_symtab("LDL" ,"08");
+	add_to_symtab("LDX" ,"04");
+	add_to_symtab("MUL" ,"20");
+	add_to_symtab("OR"  ,"44");
+	add_to_symtab("RD"  ,"D8");
+	add_to_symtab("RSUB","4C");
+	add_to_symtab("STA" ,"0C");
+	add_to_symtab("STCH","54");
+	add_to_symtab("STL" ,"14");
+	add_to_symtab("STX" ,"10");
+	add_to_symtab("SUB" ,"1C");
+	add_to_symtab("TD"  ,"E0");
+	add_to_symtab("TIX" ,"2C");
+	add_to_symtab("WD"  ,"DC");
 }
--- symtab.h
+++ symtab.h
@@ -5,6 +5,7 @@
 
 void initialize();
 bool add_to_symtab(std::string symbol, std::string value);
-bool read_from_symtab(std::string symbol, std::string &value);
+bool read_from_symtab(std::string symbol, std::string &value); // Gets value which matches symbol
+bool find_from_symtab(std::string &symbol, std::string value); // Finds symbol which matches value
 
 #endif
